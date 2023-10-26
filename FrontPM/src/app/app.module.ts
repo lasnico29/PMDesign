266f3fee52e9d1ff6054911366509eaa4c43af68
LuchoZ22 <lfzm2273@gmail.com>
2023-10-26 09:45:46 -0400

    Aance
    clases

--- FrontPM/src/app/app.module.ts
+++ FrontPM/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
